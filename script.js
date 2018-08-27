885f7fa55bfe70727f8a3548dc629e2f27814cbf
Jo King <joozooh@gmail.com>
2018-08-26 22:51:29 -0700

    Create function to populate sketch grid
    With this commit a function is written in the script(.js) to create a
    grid of divs.
    It does this by taking a number as its argument and then looping to
    create divs, which it then appends as a child to the .container div
    in the DOM.
    
    A css class is also appended to each div in the script function.
    This class is currently used, just as a test to see that the divs
    actually do appear visibly in the web-page.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test")
+sketchBoard = document.querySelector(".container");
+
+function createGrid(numberOfTiles) {
+  for (let rowI = 0; rowI < numberOfTiles; rowI++) {
+    tileRow = []
+    for (let colI = 0; colI < numberOfTiles; colI++) {
+      tile = document.createElement("div")
+      tile.classList.add("tile", `row${rowI}`, `col${colI}`)
+      tileRow.push(tile);
+    }
+    tileRow.forEach(tile => sketchBoard.appendChild(tile));
+  }
+}
+
+createGrid(16);
